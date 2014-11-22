919a7e0c8a54e7e95fe0bd59d9726071ed118b4d
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-11-22 16:37:22 +0900

    LED制御

--- totsuzen_shi.rb
+++ totsuzen_shi.rb
@@ -3,13 +3,35 @@ require 'serialport'
 module Lita
   module Handlers
     class Suddenly < Handler
-      route /^temp$/, :suddenly_death
+      route /^temp$/, :temparature
+      route /^light status$/, :light_status
+      route /^light on$/, :light_on
+      route /^light off$/, :light_off
 
-      def suddenly_death(response)
+      def temparature(response)
         sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
-        sp.puts "1"
+        sp.puts "t"
         line = sp.gets # read
         response.reply line
+      end
+
+      def light_status(response)
+        sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+        sp.puts "l"
+        line = sp.gets # read
+        response.reply line
+      end
+
+      def light_on(response)
+        sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+        sp.puts "1"
+        response.reply "light on"
+      end
+
+      def light_off(response)
+        sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+        sp.puts "0"
+        response.reply "light off"
       end
     end
     Lita.register_handler(Suddenly)
