5772711b3efb8ec8a5beeaca6247537f1f9cf52e
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-11-22 16:37:51 +0900

    タイマー処理

--- totsuzen_shi.rb
+++ totsuzen_shi.rb
@@ -4,6 +4,7 @@ module Lita
   module Handlers
     class Suddenly < Handler
       route /^temp$/, :temparature
+      route /^start temp$/, :reminder
       route /^light status$/, :light_status
       route /^light on$/, :light_on
       route /^light off$/, :light_off
@@ -32,6 +33,15 @@ module Lita
         sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
         sp.puts "0"
         response.reply "light off"
+      end
+
+      def reminder(response)
+        every(10) do |timer|
+          sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+          sp.puts "t"
+          line = sp.gets # read
+          response.reply line + "by timer"
+        end
       end
     end
     Lita.register_handler(Suddenly)
