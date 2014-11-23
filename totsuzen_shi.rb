5a14b6b01ea3938104805cc064263304559273a1
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-11-23 23:35:15 +0000

    refactoring

--- totsuzen_shi.rb
+++ totsuzen_shi.rb
@@ -2,7 +2,8 @@ require 'serialport'
 
 module Lita
   module Handlers
-    class Suddenly < Handler
+    class WrbbHandler < Handler
+      DEVICE = '/dev/ttyACM0'
       route /^temp$/, :temparature
       route /^start temp$/, :reminder
       route /^light status$/, :light_status
@@ -10,40 +11,40 @@ module Lita
       route /^light off$/, :light_off
 
       def temparature(response)
-        sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+        sp = SerialPort.new(@device_name, 9600, 8, 1, 0) 
         sp.puts "t"
         line = sp.gets # read
         response.reply line
       end
 
       def light_status(response)
-        sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+        sp = SerialPort.new(@device_name, 9600, 8, 1, 0) 
         sp.puts "l"
         line = sp.gets # read
         response.reply line
       end
 
       def light_on(response)
-        sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+        sp = SerialPort.new(@device_name, 9600, 8, 1, 0) 
         sp.puts "1"
         response.reply "light on"
       end
 
       def light_off(response)
-        sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+        sp = SerialPort.new(@device_name, 9600, 8, 1, 0) 
         sp.puts "0"
         response.reply "light off"
       end
 
       def reminder(response)
         every(10) do |timer|
-          sp = SerialPort.new('/dev/tty.usbmodem1.1', 9600, 8, 1, 0) 
+          sp = SerialPort.new(@device_name, 9600, 8, 1, 0) 
           sp.puts "t"
           line = sp.gets # read
           response.reply line + "by timer"
         end
       end
     end
-    Lita.register_handler(Suddenly)
+    Lita.register_handler(WrbbHandler)
   end
 end
